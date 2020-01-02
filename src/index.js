dbf5c4b91dbb62fc6e401761cff16f9e1030c6c6
samuelguedesalves <samuelguedes7070@gmail.com>
2020-01-02 16:16:06 -0400

    new user router update

--- src/index.js
+++ src/index.js
@@ -25,17 +25,22 @@ api.post('/login', (req, res)=>{
     if(email != "" && password != ""){
         axios.get('https://carrot-9b7e4.firebaseio.com/Carrot/Users.json')
             .then((response_database) => {
+                var state = false;
                 for(var i in response_database.data){
                     if(response_database.data[i].email === email){
                         if(response_database.data[i].password === password){
+                            state = true;
                             res.send({log: true, username: response_database.data[i].name, userId: i });
                         }else{
+                            state = true;
                             res.send({ log: false });
                         }
                     }
                 }
 
-                res.send({ log: false });
+                if(!state){
+                    res.send({ log: false });
+                }
 
             })
             .catch((error_database) =>{
@@ -46,7 +51,8 @@ api.post('/login', (req, res)=>{
     }
 });
 
-//rota de cadastro
+
+//signup user router
 api.post('/new_user', (req, res)=>{
     const name = req.body.name_user;
     const email = req.body.email_user;
@@ -56,46 +62,41 @@ api.post('/new_user', (req, res)=>{
         
         axios.get('https://carrot-9b7e4.firebaseio.com/Carrot/Users.json')
             .then((response) => {
+                var state = false;
+
                 for(var i in response.data){
-                    console.log(response.data[i].email);
-                    if(response.data[i].email == email){
+                    if(response.data[i].email === email){
+                        state = true;
                         res.send({ insert: false });
                     };  
                 }
 
-                axios.post('https://carrot-9b7e4.firebaseio.com/Carrot/Users.json', {
-                        name: name,
-                        email: email,
-                        password: password,
-                    })
-                    .then((data) => {
-                        res.send({ insert: true });
-                    })
-                    .catch((err) => {
-                        res.send({ insert: false });
-                    });
+                if(!state){
+                    axios.post('https://carrot-9b7e4.firebaseio.com/Carrot/Users.json', {
+                            name: name,
+                            email: email,
+                            password: password,
+                        })
+                        .then((data) => {
+                            res.send({ insert: true });
+                        })
+                        .catch((err) => {
+                            res.send({ insert: false });
+                        });
+                }
 
             })
             .catch((err)=>{
                 res.send({ insert: false });
             });
+    }else{
+        res.send({ insert: false });
     }
 });
 
 // rota de listagem de produtos
-api.get('/product_list', (req, res)=>{
-    /*
-    connection.query('call listar_produtos()',(err, result)=>{
-        if (err) res.sendStatus(500);
-        res.send(result[0]);
-    });
-    */
-   res.send({products: [
-       {
-           product_name: 'banana',
-           product_value: 2.5,
-       }
-   ]})
+api.get('/my_task', (req, res)=>{
+    
 });
 
 function countObject(obj){
